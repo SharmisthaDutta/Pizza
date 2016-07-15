551fec725ba1b7e6d31e75ef4f5d4c23af7dd0d3
SharmisthaDutta <dutta@pdx.edu>
2016-07-14 21:24:45 -0700

    Pizza and Drink Menu view -- not very confident about quality of design

--- app/src/main/java/com/example/sharmistha/pizza/PizzaAndDrinkMenuActivity.java
+++ app/src/main/java/com/example/sharmistha/pizza/PizzaAndDrinkMenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.sharmistha.pizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PizzaAndDrinkMenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class PizzaAndDrinkMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_and_drink_menu);
+        Button confirm_order_button = (Button) findViewById(R.id.confirm_order_button);
+        Button pizza_drink_menu_button = (Button) findViewById(R.id.pizza_drink_menu_button);
+
+        //when click on confirm order button review update order page is displayed
+        confirm_order_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent =new Intent(getApplicationContext(),ReviewUpdateOrderActivity.class);
+                startActivity(intent);
+            }
+        });
+        //when click on add order button pizza and drink page should be shown again with previous entries
+        // erased and previous entered data should be stored in db
+        pizza_drink_menu_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent =new Intent(getApplicationContext(),PizzaAndDrinkMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
